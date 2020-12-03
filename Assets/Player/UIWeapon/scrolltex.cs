b80e99df068070b0933459fec6b1452fa4c13559
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-12-03 12:53:09 +0000

    moving line texture
    need to attach to weapon next

---
+++ Assets/Player/UIWeapon/scrolltex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrolltex : MonoBehaviour
+{
+    [SerializeField] private Vector2 scrollSpeed = new Vector2(.5f, .5f);
+    private Renderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * scrollSpeed.x, Time.time * scrollSpeed.y);
+        _renderer.material.mainTextureOffset = offset;
+    }
+}
