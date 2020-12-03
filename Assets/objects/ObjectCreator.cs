e25a3344f1065dc0f10e3ba9e7ff01e9e6f5aae2
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-12-03 23:16:17 +0000

    trying to load all objects in a folder

---
+++ Assets/objects/ObjectCreator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class ObjectCreator : MonoBehaviour
+{
+    private Object[] models;
+
+    void Start()
+    {
+        models = Resources.LoadAll("obj", typeof(Object));
+
+        if(models.Length < 3) Debug.Log("Fucked up");
+        
+        foreach (var t in models)
+        {
+            Debug.Log(t.name);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("K pressed");
+            int index = UnityEngine.Random.Range(0, models.Length);
+            GameObject obj = (GameObject)Instantiate(models[index], transform.position, transform.rotation);
+        }
+    }
+}
