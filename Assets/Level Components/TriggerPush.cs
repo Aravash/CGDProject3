2a9f1a1b2b7d451e1ef24bd7868602b6aee128d9
Hosomi <surendaotoko@gmail.com>
2020-12-04 16:38:12 +0000

    Add TriggerPush

---
+++ Assets/Level Components/TriggerPush.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPush : MonoBehaviour
+{
+    [SerializeField] Vector3 dir = Vector3.forward;
+    [SerializeField] float force = 1;
+
+    private void Start()
+    {
+        dir.Normalize();
+        dir *= force;
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        Rigidbody other = col.GetComponent<Rigidbody>();
+        if (other)
+        {
+            other.AddForce(dir, ForceMode.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, dir.normalized);
+    }
+}
