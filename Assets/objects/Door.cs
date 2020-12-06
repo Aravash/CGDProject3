6af146989be528f1819958f2703f8091be05312e
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-12-06 18:54:28 +0000

    added pizza chair object and door

---
+++ Assets/objects/Door.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private bool isOpen = false;
+    [SerializeField] private float hingeSpeed = 15f;
+    private Transform hinge;
+    private BoxCollider exitZone;
+
+    private void Start()
+    {
+        exitZone = GetComponent<BoxCollider>();
+        hinge = transform.GetChild(0).GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen)
+        {
+            if(transform.rotation.y <= 90)
+            {
+                hinge.transform.Rotate(0, Time.deltaTime * hingeSpeed, 0);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            CanvasManager.loadMenu();
+    }
+
+    public void open()
+    { 
+        isOpen = true; 
+        exitZone.enabled = true;
+    }
+}
