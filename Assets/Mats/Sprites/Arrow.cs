86b8b438874ee4bae8a9419902b6b460d5afea79
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-12-06 16:28:23 +0000

    arrow bob and geometry

--- Assets/Mats/Sprites/Arrow.cs
+++ Assets/Mats/Sprites/Arrow.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform playerCam;
+    private float maxBobTime = .5f;
+    private float bobTime = .5f;
+    private int dir = -1;
+    public float speed = .5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(playerCam);
+        bob();
+    }
+
+    void bob()
+    {
+        if (bobTime <= 0)
+        {
+            bobTime = maxBobTime;
+            dir *= -1;
+        }
+        else bobTime -= Time.deltaTime;
+
+        transform.position = Vector3.Lerp( transform.position, transform.position + Vector3.up * dir, Time.deltaTime * speed);
     }
 }
