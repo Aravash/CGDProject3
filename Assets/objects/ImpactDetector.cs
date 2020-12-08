67e4fe276bce737850b6ea6573c18b3e0cb5a2a7
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-12-08 18:08:33 +0000

    Wrapped objects use the wrapping's collision

---
+++ Assets/objects/ImpactDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactDetector : MonoBehaviour
+{
+    public bool allowBreaking = false;
+    [SerializeField] private float impactBreakForce = 2;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("velocity is " + GetComponent<Rigidbody>().velocity.magnitude + ", allow breaking is " + allowBreaking);
+        // Detecting hard impact
+        if (GetComponent<Rigidbody>().velocity.magnitude >= impactBreakForce && allowBreaking)
+        {
+            Debug.Log("pop condition met");
+            transform.GetChild(0).GetComponent<WrappingHandler>().PopWrapping(GetComponent<Rigidbody>().velocity);
+        }
+
+        allowBreaking = false;
+    }
+}
--- Assets/objects/WrappingHandler.cs
+++ Assets/objects/WrappingHandler.cs
@@ -4,42 +4,34 @@ using UnityEngine;
 
 public class WrappingHandler : MonoBehaviour
 {
-    public bool allowBreaking = false;
-
     //private Collider transCol;
     private Rigidbody rb;
-    [SerializeField] private float impactBreakForce = 5;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        Destroy(GetComponent<Rigidbody>());
+        GetComponent<MeshCollider>().enabled = false;
         GetComponent<MeshRenderer>().enabled = false;
 
         // Add wrapping around object
-        GameObject wrapping = Instantiate(Resources.Load("Wrapping/Wrapping"), transform) as GameObject;
+        GameObject wrapping = Instantiate(Resources.Load("Wrapping/Wrapping"), transform.position, transform.rotation) as GameObject;
+        rb = wrapping.GetComponent<Rigidbody>();
+        transform.SetParent(wrapping.transform);
         wrapping.name = "Wrapping";
-
-        // Resize wrapping
-        //transCol = transform.transform.GetComponent<Collider>();
-        wrapping.transform.Find("Mesh").GetComponent<Collider>().enabled = false;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        // Detecting hard impact
-        if (rb.velocity.magnitude >= impactBreakForce && allowBreaking)
-        {
-            PopWrapping();
-        }
         
-        allowBreaking = false;
+        //wrapping.transform.Find("Mesh").GetComponent<Collider>().enabled = false;
     }
 
-    void PopWrapping()
+    public void PopWrapping(Vector3 newVelocity)
     {
+        Debug.Log("pop wrapping called");
         //shread wrapping (remove mesh layer, create pop particles, etc.) & remove this script
+        GetComponent<MeshCollider>().enabled = true;
         GetComponent<MeshRenderer>().enabled = true;
-        Destroy(transform.Find("Wrapping").gameObject);
+        gameObject.AddComponent<Rigidbody>();
+        GetComponent<Rigidbody>().velocity = newVelocity;
+        Destroy(transform.parent.gameObject);
+        gameObject.transform.SetParent(null);
         Destroy(this);
     }
 }
